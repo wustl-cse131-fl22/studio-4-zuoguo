941b92beb710163b5f6f648e53217ca285632079
Alex <Alex@LAPTOP-23MLK1MJ.dhcp.wustl.edu>
2022-10-13 12:40:43 -0500

    10/13

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -2,10 +2,22 @@ package studio4;
 
 import java.awt.Color;
 
-import edu.princeton.cs.introcs.StdDraw;
 
+import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
+
 	public static void main(String[] args) {
+		StdDraw.setXscale(0, 22);
+		StdDraw.setYscale(0, 16);
+		StdDraw.setPenColor(255, 0, 0);
+		StdDraw.filledRectangle(3, 3, 3, 3);
+		StdDraw.filledRectangle(3, 13, 3, 3);
+		StdDraw.filledRectangle(16, 3, 6, 3);
+		StdDraw.filledRectangle(16, 13, 6, 3);
+		StdDraw.setPenColor(255, 109, 182);
+		StdDraw.filledRectangle(11, 8, 11, 1);
+		StdDraw.filledRectangle(8, 8, 1, 8);
+	
 		
 	}
-}
+	}
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,24 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String type = in.next();
+		int x1 = in.nextInt();
+		int x2 = in.nextInt();
+		int x3 = in.nextInt();
+		Boolean T1 = in.nextBoolean();
+		double x4 = in.nextDouble();
+		double x5 = in.nextDouble();
+		double x6 = in.nextDouble();
+		double x7 = in.nextDouble();
+		Color purple = new Color(x1, x2, x3);
+		StdDraw.filledEllipse(x4, x5, x6, x7);
+
+
+		
+	}
+
+	private static void filledEllipse(double x4, double x5, double x6, double x7) {
+		// TODO Auto-generated method stub
 		
 	}
 }
